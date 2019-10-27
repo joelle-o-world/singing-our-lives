05657c6c74cca3526d66ec8fecbea86ac50f483a
Sean Norris <seandeclannorris@gmail.com>
2019-10-27 18:34:51 +0000

    Trying to rewrite RecorderInterface using MediaStream. (nonfunctional)

--- src/RecorderInterface.js
+++ src/RecorderInterface.js
@@ -1,9 +1,8 @@
-import {AudioIn, SoundFile, SoundRecorder} from 'p5';
-import 'p5/lib/addons/p5.sound';
-
-const micRoberts = new AudioIn();
-console.log('p5 audio in:', micRoberts);
-
+//October 27, 2019
+//Rewriting RecorderInterface using Mozilla MediaStream Recorder API
+//https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Using_the_MediaStream_Recording_API
+var mediaRecorder;
+var audio_samples = [];
 class RecorderInterface {
   constructor(){
     this.makeHTML();
@@ -85,44 +84,77 @@ class RecorderInterface {
 
   record() {
     console.log("## Calling record()");
+    this.updateState('recording');
 
-    if(micRoberts.enabled){//only start recording audio if the AudioIn is enabled
-      this.recorder = new SoundRecorder();
-      this.recorder.setInput(micRoberts);
-      this.soundFile = new SoundFile();
-      this.recorder.record(this.soundFile);
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {//Check whether getUserMedia is supported by the browser before running anything else
+      console.log('getUserMedia supported.');//Print if it is supported
+      navigator.mediaDevices.getUserMedia (
+        {
+          audio: true
+        })
 
-      this.updateState("recording");
-    } else {
-      micRoberts.start(
-        () => {
-          // Success :)
-          console.log('## User enabled audio..')
-          this.record();
-        },
-        () => {
-          // Failure :(
-          console.warn("AudioIn.start failed.")
+        .then(function(stream) { //execute if successful
+          console.log("## Stream sucessfully established. Recording...");
+
+          mediaRecorder = new MediaRecorder(stream);//pass the stream into new MediaRecorder()
+
+          mediaRecorder.start();//start recording
+
+
+
+          //event handler, executed whenever new data is available from the MediaRecorder
+          mediaRecorder.ondataavailable = function(e){//***this is only being called once?? so this still doesn't work
+            console.log('dataavailable');
+            audio_samples.push(e.data);
+          }
+
+          //event handler, executed when MediaRecorder.stop() is called:
+          mediaRecorder.onstop = function(e){
+            console.log('## mediaRecorder.onstop event ocurred.');
+            console.log('audio_samples.length: ' + audio_samples.length);
+            //make an audio element to hold the recorded stream:
+
+            //test: automatically download the recorded audio when stream is stopped:
+            var blob = new Blob(audio_samples, { 'type' : 'audio/ogg; codecs=opus' });
+            var url = URL.createObjectURL(blob);
+            var a = document.createElement("a");
+            document.body.appendChild(a);
+            a.style = "display: none";
+            a.href = url;
+            a.download = "test.webm";
+            a.click();
+            window.URL.revokeObjectURL(url);
+          }
+
         }
-      )
-    }
+
+      })
+
+      .catch(function(err) {//execute if error
+        console.log('The following getUserMedia error occured: ' + err);
+      }
+    );
+  } else {
+    console.log('getUserMedia not supported on your browser!'); //Print if getUserMedia is not supported
   }
 
-  stop() {
-    console.log("## Calling stop()");
-    this.recorder.stop();
-    this.updateState("recorded");
-  }
+}
 
-  play() {
-    console.log("## Calling play()")
-    console.log("Sound file:", this.soundFile)
-    this.soundFile.play();
+stop() {
+  console.log("## Calling stop()");
+  mediaRecorder.stop();
+  this.updateState("recorded");
+}
 
-    this.updateState("playing");
+play() {
+  console.log("## Calling play()")
+  // console.log("Sound file:", this.soundFile)
+  // this.soundFile.play();
 
-    console.log("TODO: Set state back to 'recorded' once playback finishes.")
-  }
+  this.updateState("playing");
+
+  console.log("TODO: Set state back to 'recorded' once playback finishes.")
+}
 }
 
 export {RecorderInterface}
