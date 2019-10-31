22498582562321ede64bc5b6782f8e833842f908
Joel Plowright <joelplow@gmail.com>
2019-10-31 20:10:58 +0000

    swt up git pulling and pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,22 @@
 module.exports = {
   apps: [{
     name: "singing-our-lives",
-    script: "server/index.js",
-    watch: ["server", "public"],
+    script: "npm",
+    args: "start",
+    interpreter: 'none',
+    watch: true,
     // Delay between restart
     watch_delay: 1000,
-    ignore_watch : ["node_modules"],
+    ignore_watch : ["node_modules", "formfills"],
     watch_options: {
       "followSymlinks": false
     }
+  },
+  {
+    name: "git-watcher",
+    script: "watchGithub.sh",
+    watch_delay: 60000,
+    watch: true,
+    ignore_watch : ["node_modules", "formfills"]
   }]
 }
---
+++ watchGithub.sh
@@ -0,0 +1,7 @@
+while true;
+do
+  git fetch origin master;
+  git pull;
+
+  sleep 60;
+done;
