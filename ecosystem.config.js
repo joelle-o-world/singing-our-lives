c2a520375bc184d1a18dd8af13a780592c96088c
Joel Plowright <joelplow@gmail.com>
2019-10-31 14:37:39 +0000

    add pm2 config to watch for changes

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    script: "server/index.js",
+    watch: ["server", "public"],
+    // Delay between restart
+    watch_delay: 1000,
+    ignore_watch : ["node_modules"],
+    watch_options: {
+      "followSymlinks": false
+    }
+  }]
+}
