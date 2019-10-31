6c9bd32a041cf195a07cbc8486254481fb9a3f1c
Joel Plowright <joelplow@gmail.com>
2019-10-31 16:13:23 +0000

    updateFromGithub experimental shellscript

---
+++ updateFromGithub.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# fetch branches from 
+git fetch origin master
+if git diff origin/master
+then 
+  echo "Applying update from github...";
+  git pull;
+  npx webpack;
+  pm2 restart "Singing our lives"
+else
+  echo "No changes on github."
+fi
