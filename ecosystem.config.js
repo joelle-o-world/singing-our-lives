30070143a9a156805c790b6df9fcf0cfac36fa5f
Joel Plowright <joelplow@gmail.com>
2019-11-15 13:44:25 +0000

    pm2 dont watch anything

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,19 +4,12 @@ module.exports = {
     script: "npm",
     args: "start",
     interpreter: 'none',
-    watch: true,
-    // Delay between restart
-    watch_delay: 1000,
-    ignore_watch : ["node_modules", "formfills"],
-    watch_options: {
-      "followSymlinks": false
-    }
   },
-  {
+  /*{
     name: "git-watcher",
     script: "watchGithub.sh",
     watch_delay: 60000,
     watch: true,
     ignore_watch : ["node_modules", "formfills", 'package-lock.json']
-  }]
+  }]*/
 }
