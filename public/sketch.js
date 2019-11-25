fc96db8f747f96badbd74322acf7d35a6f7204b6
Joel Plowright <joelplow@gmail.com>
2019-11-25 02:07:50 +0000

    moved p5 waveform

--- public/sketch.js
+++ public/sketch.js
@@ -16,7 +16,7 @@ function setup() {
 
 let isVisualiserRunning = false;
 function setup_visualiser(){
-  canvas = createCanvas(250, 75);
+  canvas = createCanvas(windowWidth,300);
   canvas.parent('p5canvas');
 
   audio_segs = 21;
