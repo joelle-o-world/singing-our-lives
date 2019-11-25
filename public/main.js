547e74ce9c47923bc3fae2c144e883919a0486a4
Joel Plowright <joelplow@gmail.com>
2019-11-25 01:59:13 +0000

    scroll to top on button click

--- public/main.js
+++ public/main.js
@@ -55,6 +55,10 @@ function showPage(pageIndexOrID) {
     // Open a page using a numeric id.
     pages[pageIndexOrID].hidden = false;
   }
+
+  // Scroll to top
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 function finishedRecording() {
